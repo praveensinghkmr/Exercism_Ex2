7a57e74bb2b4d9737013852d8e718d3faa1a01e6
Praveenkumar Singh <praveenkumar.singh@in.abb.com>
2018-09-18 19:18:08 +0530

    Implemented Leap Year Logic

--- Leap.cs
+++ Leap.cs
@@ -5,7 +5,10 @@ public static class Leap
     public static bool IsLeapYear(int year)
     {
 
-        //Implementation
+        if (year % 400 == 0)
+            return true;
+        else if ((year % 4 == 0) && (year % 100 != 0))
+            return true;
         return false;
     }
 }
